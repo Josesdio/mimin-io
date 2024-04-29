fffaf8aa92eb536bf4e4d33e93ed89b1b8e9fffa
Josesdiogit config --global user.email josesdio948@gmail.com <josesdio948@gmail.com>
2024-04-29 13:27:54 +0700

    Commit perubahan yang dimodifikasi

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
-// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+}
 
-const securityHeaders = [
-    {
-    source: '/(.*)',
-    headers: [
-        {
-        key: 'Cache-Control',
-        value: 'public, max-age=3600, must-revalidate',
-        },
-    ],
-    },
-];
-
-module.exports = {
-    async headers() {
-    return securityHeaders;
-    },
-};
+module.exports = nextConfig
